4de0585114e382da7867119a2c7dc7ddff859550
Kazuhiro Kubota <k2.wanko@gmail.com>
2017-11-23 15:38:34 +0900

    Update node_client

--- node_client/main.js
+++ node_client/main.js
@@ -1,26 +1,21 @@
-const messages = require('../echo/echo_pb')
-const services = require('../echo/echo_pb_service')
+const {EchoHistoryRequest} = require('../echo/echo_pb')
+const {EchoService} = require('../echo/echo_pb_service')
 
 const {grpc, Code} = require('grpc-web-client')
 
 const host = 'https://gae-grpc-web.appspot.com:443'
 
 function main() {
-  const request = new messages.EchoRequest()
-  request.setMessage('Hello')
-  grpc.unary(services.EchoService.Echo, {
+  const request = new EchoHistoryRequest()
+  request.setLimit(10)
+  grpc.invoke(EchoService.EchoHistory, {
     request,
     host,
-    onEnd: res => {
-      const { status, statusMessage, headers, message, trailers } = res
-      console.log("EchoService.Echo.onEnd.status", status, statusMessage)
-      console.log("EchoService.Echo.onEnd.headers", headers)
-      if (status === Code.OK && message) {
-        const resp = message.toObject()
-        console.log("EchoService.Echo.onEnd.message", resp)
-      }
-      console.log("EchoService.Echo.onEnd.trailers", trailers)
-    }
+    onMessage: msg => {
+      const resp = msg.toObject()
+      console.log("fetchEchoHistory", "onMessage", resp)
+    },
+    onEnd: () => {}
   })
 }
 
